50bdc21b6757696cb3ab744fd0a6988d6a2ee561
akbaraliev-7 <60475565+akbaraliev-7@users.noreply.github.com>
2023-03-05 00:09:55 +0600

    Add files via upload

---
+++ form.java
@@ -0,0 +1,63 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+public class form extends JFrame {
+    private JTextField textField1;
+    private JButton button1;
+    private JTextField textField2;
+    private JPanel panel;
+
+    public form() {
+        panel = new JPanel();
+        panel.setLayout(new BoxLayout(panel, BoxLayout.PAGE_AXIS));
+        panel.setBorder(BorderFactory.createEmptyBorder(20, 20, 20, 20));
+        textField1 = new JTextField();
+        textField1.setMaximumSize(new Dimension(Integer.MAX_VALUE, textField1.getPreferredSize().height));
+        textField2 = new JTextField();
+        textField2.setMaximumSize(new Dimension(Integer.MAX_VALUE, textField2.getPreferredSize().height));
+        textField2.setEditable(false);
+
+        button1 = new JButton("Перевернуть");
+        button1.setAlignmentX(Component.CENTER_ALIGNMENT);
+        button1.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                String inputText = textField1.getText();
+                int midIndex = inputText.length() / 2;
+                String rightText = inputText.substring(midIndex);
+                String outputText = "";
+                if (inputText.length() % 2 == 0) {
+                    outputText = inputText.substring(midIndex - 1, midIndex + 1);
+                    for (int i = midIndex + 2; i < inputText.length(); i += 2) {
+                        if (i + 1 < inputText.length()) {
+                            outputText += inputText.substring(i, i+2);
+                        } else {
+                            outputText += inputText.substring(i);
+                        }
+                    }
+                } else {
+                    for (int i = 0; i < rightText.length(); i += 3) {
+                        if (i + 2 < rightText.length()) {
+                            outputText += rightText.substring(i, i+2);
+                        } else {
+                            outputText += rightText.substring(i);
+                        }
+                    }
+                    }
+                textField2.setText(outputText);
+            }
+        });
+        panel.add(textField1);
+        panel.add(Box.createRigidArea(new Dimension(0, 10)));
+        panel.add(button1);
+        panel.add(Box.createRigidArea(new Dimension(0, 10)));
+        panel.add(textField2);
+    }
+    public void showForm() {
+        setSize(330, 150);
+        add(panel);
+        setVisible(true);
+    }
+}
+
+
